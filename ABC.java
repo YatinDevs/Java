50f830342c60b343ce176d5583a09dceb18664f1
YatinDevs <c.yatin727@gmail.com>
2022-05-08 19:09:07 +0530

    file done

---
+++ ABC.java
@@ -0,0 +1,4 @@
+public class ABC{
+   public static void main(String ar[])
+       {System.out.println("Hello    students");
+}}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String ar[])
+              {
+                  System.out.println(" Hello students");
+                  System.out.println(" Name : Yatin Chaudhari");
+                  System.out.println(" Branch : Dac " );
+                  System.out.println(" College Name : Cdac Noida " );
+              }
+            }
